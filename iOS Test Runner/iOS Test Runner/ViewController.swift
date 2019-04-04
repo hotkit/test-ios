65c3c6a5246c6b51839c8efc072acfe80ccc21bd
Kirit Sælensminde <kirit@kirit.com>
2019-04-04 09:30:56 +0700

    Files generated by XCode.

---
+++ iOS Test Runner/iOS Test Runner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Test Runner
+//
+//  Created by Jupiter God on 04/04/2019.
+//  Copyright © 2019 HotNow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
