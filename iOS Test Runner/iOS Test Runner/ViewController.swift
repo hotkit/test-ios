7657a658a53e93ea128e395c399fb505caf1146d
Kirit Sælensminde <kirit@kirit.com>
2019-04-06 13:37:25 +0700

    Add text label and put some dummy content in it

--- iOS Test Runner/iOS Test Runner/ViewController.swift
+++ iOS Test Runner/iOS Test Runner/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var results: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        results.text = "Swift text...\nSwift text line 2"
+        results.sizeToFit()
     }
 
 
